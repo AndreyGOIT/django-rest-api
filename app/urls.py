69239e7b18ac1db04502c4798dc0bcc474330eb1
AndreyGOIT <ae.borga@gmail.com>
2025-09-08 17:37:42 +0300

    add Views.py and Urls.py definitions

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
+from . import views
+
 router = routers.DefaultRouter()
+router.register(r'opettajat', views.OpettajaViewSet, 'opettaja-nimi')
+router.register(r'kurssit', views.KurssiViewSet, 'kurssi-nimi')
 
 
 urlpatterns = [
--- app/views.py
+++ app/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Opettaja, Kurssi
+from .serializers import OpettajaSerializer, KurssiSerializer
 
-# Create your views here.
+''' A.o metodi osaa tehdä kaikki CRUD operaatiot
+    GET, POST, PUT, DELETE ja hakea opettajat:
+    -kaikki opettajat: /api/opettajat/
+    -opettajan id:llä /api/opettajat/1/
+    -opettajat nimen perusteella: /api/opettajat/?nimi=joku_nimi
+'''
+
+class OpettajaViewSet(viewsets.ModelViewSet):
+    serializer_class = OpettajaSerializer
+
+    def get_queryset(self):
+        queryset = Opettaja.objects.all()
+        nimi = self.request.query_params.get('nimi')
+        if nimi is not None:
+            queryset = queryset.filter(nimi=nimi)
+        return queryset
+
+''' A.o metodi osaa tehdä kaikki CRUD operaatiot 
+    GET, POST, PUT, DELETE ja hakea kurssit:
+    -kaikki kurssit: /api/kurssit/
+    -kurssi id:llä /api/kurssit/1/
+    -kurssit nimen perusteella: /api/kurssit/?nimi=joku_nimi
+    -kurssit opettajan id:llä /api/kurssit/?opettaja=opettaja_id
+'''
+class KurssiViewSet(viewsets.ModelViewSet):
+    serializer_class = KurssiSerializer
+
+    def get_queryset(self):
+        queryset = Kurssi.objects.all()
+        nimi = self.request.query_params.get('nimi')
+        opettaja = self.request.query_params.get('opettaja')
+        if nimi is not None:
+            queryset = queryset.filter(nimi=nimi)
+        if opettaja is not None:
+            queryset = queryset.filter(opettaja=opettaja)
+        return queryset
